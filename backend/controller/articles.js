5f3cecf5a7b40217ca1467026ab3edb4deac1f2b
Meisen Wang <bigmasonwang@gmail.com>
2021-08-26 11:22:12 +0930

    🦄 refactor(controller): extract controller

---
+++ backend/controller/articles.js
@@ -0,0 +1,59 @@
+const moment = require('moment');
+const Article = require('../models/article-model');
+
+//GET '/articles'
+const getAllArticles = (req, res, next) => {
+  Article.find((err, articles) => {
+    if (err) {
+      console.error(err);
+      return res.sendStatus(404);
+    }
+    return res.status(200).send(articles);
+  });
+};
+
+//POST '/articles'
+const newArticle = (req, res, next) => {
+  const article = new Article({
+    created_at: moment().format('MMMM Do YYYY, h:mm:ss a').toString(),
+    updated_at: moment().format('MMMM Do YYYY, h:mm:ss a').toString(),
+    ...req.body,
+  });
+  article.save((err) => {
+    if (err) {
+      console.error(err);
+      return res.send(err);
+    }
+    return res.sendStatus(201);
+  });
+};
+
+//DELETE '/articles'
+const deleteAllArticles = (req, res, next) => {
+  res.json({ message: 'DELETE all articles' });
+};
+
+//GET '/articles/:name'
+const getOneArticle = (req, res, next) => {
+  res.json({ message: 'GET 1 article' });
+};
+
+//POST '/articles/:name'
+const newComment = (req, res, next) => {
+  res.json({ message: 'POST 1 article comment' });
+};
+
+//DELETE '/articles/:name'
+const deleteOneArticle = (req, res, next) => {
+  res.json({ message: 'DELETE 1 article' });
+};
+
+//export controller functions
+module.exports = {
+  getAllArticles,
+  newArticle,
+  deleteAllArticles,
+  getOneArticle,
+  newComment,
+  deleteOneArticle,
+};
--- backend/routes/articles.js
+++ backend/routes/articles.js
@@ -1,31 +1,9 @@
 const express = require('express');
-const moment = require('moment');
 
-const Article = require('../models/article-model');
+const ArticlesController = require('../controller/articles');
 
 const router = express.Router();
-router.get('/', (req, res, next) => {
-  Article.find((err, articles) => {
-    if (err) {
-      console.error(err);
-      return res.sendStatus(404);
-    }
-    return res.status(200).send(articles);
-  });
-});
-router.post('/', (req, res) => {
-  const article = new Article({
-    created_at: moment().format('MMMM Do YYYY, h:mm:ss a').toString(),
-    updated_at: moment().format('MMMM Do YYYY, h:mm:ss a').toString(),
-    ...req.body,
-  });
-  article.save((err) => {
-    if (err) {
-      console.error(err);
-      return res.send(err);
-    }
-    return res.sendStatus(201);
-  });
-});
+router.get('/', ArticlesController.getAllArticles);
+router.post('/', ArticlesController.newArticle);
 
 module.exports = router;
